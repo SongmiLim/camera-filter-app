05c8f29cc905824e8264d37697be29c7c4e2d5fd
songmiLim <lsmi00121@gmail.com>
2025-04-14 00:16:05 +0900

    [Feat] Add Camera logic(2)

--- app/src/main/assets/shaders/brightness.fs
+++ app/src/main/assets/shaders/brightness.fs
@@ -1,4 +1,3 @@
-// assets/shaders/brightness.fs
 #extension GL_OES_EGL_image_external : require
 precision mediump float;
 
@@ -7,5 +6,7 @@ varying vec2 vTexCoord;
 
 void main() {
     vec4 color = texture2D(uTexture, vTexCoord);
-    gl_FragColor = vec4(min(color.rgb + 0.2, 1.0), 1.0);
+    float brightness = 0.5;
+    color.rgb += brightness;
+    gl_FragColor = vec4(color.rgb, 1.0);
 }
--- app/src/main/assets/shaders/grayscale.fs
+++ app/src/main/assets/shaders/grayscale.fs
@@ -1,4 +1,3 @@
-// assets/shaders/grayscale.fs
 #extension GL_OES_EGL_image_external : require
 precision mediump float;
 
@@ -7,6 +6,6 @@ varying vec2 vTexCoord;
 
 void main() {
     vec4 color = texture2D(uTexture, vTexCoord);
-    float gray = dot(color.rgb, vec3(0.299, 0.587, 0.114));
+    float gray = (color.r + color.g + color.b) / 3.0;
     gl_FragColor = vec4(vec3(gray), 1.0);
 }
